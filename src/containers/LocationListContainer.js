ed453cf242527b296ec7cc8837cc63b3f561bdfb
Fabian Varón <fabianvaron@MacBook-Pro-de-Fabian.local>
2019-04-06 02:11:12 -0500

    spreadoperator en reducer soluciona el bug de ciudades, uso bindActionCreators como refactor

--- src/containers/LocationListContainer.js
+++ src/containers/LocationListContainer.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {setSelectedCity, setWeather} from './../actions';
-import {getWeatherCities} from './../reducers';
+import * as actions from './../actions';
+import {getWeatherCities, getCity} from './../reducers';
 import LocationList from './../components/LocationList';
 
 class LocationListContainer extends Component {
 	componentDidMount() {
-		this.props.setWeather(this.props.cities);
+		const {setWeather, setSelectedCity, cities, city} = this.props;
+		setWeather(cities);
+		setSelectedCity(city);
 	}
 	handleLocation = city =>{
-    	this.props.setCity(city);
+    	this.props.setSelectedCity(city);
  	}	
 	render(){
 		return (
@@ -23,17 +26,24 @@ class LocationListContainer extends Component {
 }
 
 LocationListContainer.protoTypes = {
-	setCity: PropTypes.func.isRequired,
+	setSelectedCity: PropTypes.func.isRequired,
+	setWeather:  PropTypes.func.isRequired,
 	cities: PropTypes.array.isRequired,
 	citiesWeather: PropTypes.array,
+	city: PropTypes.string,
 }
 
-const mapDispatchToPropsActions = dispatch => (
+const mapDispatchToPropsActions = dispatch  => bindActionCreators(actions, dispatch);
+/*const mapDispatchToPropsActions = dispatch => (
   {
   setCity: value => dispatch(setSelectedCity(value)),
   setWeather: cities => dispatch(setWeather(cities)),
   }
-);
+);*/
 
-const mapStateToProps = state =>({citiesWeather: getWeatherCities(state)});
+const mapStateToProps = state =>({
+	citiesWeather: getWeatherCities(state),
+	city: getCity(state),
+});
+
 export default connect(mapStateToProps, mapDispatchToPropsActions)(LocationListContainer);
